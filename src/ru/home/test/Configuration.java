3eec1692417521a65a7919abed9eb76c894256f4
glofort <47744759+glofort@users.noreply.github.com>
2024-02-05 13:50:13 +0500

    'Рефактор'

--- src/ru/home/test/Configuration.java
+++ src/ru/home/test/Configuration.java
@@ -3,7 +3,13 @@ package ru.home.test;
 import java.io.File;
 import java.text.NumberFormat;
 import java.text.ParsePosition;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
